5772104d0eecdf6db4b0372e6cd797e46ee185cb
https://github.com/Jrtesta13/carrinho-testa.git <asdfghjklguilherme@gmail.com>
2022-09-23 20:49:14 -0300

    carrinho-testa

---
+++ js/models.js
@@ -0,0 +1,11 @@
+let modelsJson = [
+    {id:1, name:' friends invisible #55 ', img:'img/friend1.gif', price:[5000, 3000, 200.000], sizes:['1/72', '1/48', '1/32'], description:''},
+    {id:2, name:' friends invisible #555', img:'img/friend2.gif', price:[5000, 3000, 200.00], sizes:['1/72', '1/48', '1/32'], description:''},
+    {id:3, name:' friends invisible #12', img:'img/friend3.gif', price:[5000, 3000, 200.00], sizes:['1/72', '1/48', '1/32'], description:''},
+    {id:4, name:' friends invisible #5', img:'img/friend4.gif', price:[5000, 3000, 200.00], sizes:['1/72', '1/48', '1/32'], description:''},
+    {id:5, name:' friends invisible #20', img:'img/friend5.gif', price:[5000, 3000, 200.00], sizes:['1/72', '1/48', '1/32'], description:''},
+    {id:6, name:' friends invisible #13', img:'img/friend6.gif', price:[5000, 3000, 200.00], sizes:['1/72', '1/48', '1/32'], description:''},
+    {id:7, name:' friends invisible# 77', img:'img/friend7.gif', price:[5000, 3000, 200.00], sizes:['1/72', '1/48', '1/32'], description:''},
+    {id:8, name:' friends invisible 88', img:'img/friend8.gif', price:[5000, 3000, 200.00], sizes:['1/72', '1/48', '1/32'], description:'reconhecimento mundial!'}
+    
+];
